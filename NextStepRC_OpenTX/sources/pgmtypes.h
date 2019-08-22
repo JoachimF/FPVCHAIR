870f5de13907b2252240a230f385072dd4a1a669
JoachimF <franken.joachim@gmail.com>
2019-08-22 16:45:21 +0200

    Add files via upload

---
+++ NextStepRC_OpenTX/sources/pgmtypes.h
@@ -0,0 +1,41 @@
+/*
+**************************************************************************
+*                                                                        *
+*              This file is part of the NextStepRC project.              *
+*                                                                        *
+*                         Based on code named                            *
+*             OpenTx - https://github.com/opentx/opentx                  *
+*                                                                        *
+*              ATmega AVR family (M64, M128, M2560, M2561)               *
+*                                                                        *
+*   NextStepRC is free software: you can redistribute it and/or modify   *
+*   it under the terms of the GNU General Public License as published by *
+*   the Free Software Foundation, either version 2 of the License, or    *
+*   (at your option) any later version.                                  *
+*                                                                        *
+*   NextStepRC is distributed in the hope that it will be useful,        *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+*   GNU General Public License for more details.                         *
+*                                                                        *
+*       License GPLv2: http://www.gnu.org/licenses/gpl-2.0.html          *
+*                                                                        *
+**************************************************************************
+*/
+
+
+#ifndef __PGMTYPES__
+#define __PGMTYPES__
+
+typedef void      pm_void;
+typedef char      pm_char;
+typedef unsigned char pm_uchar;
+typedef int8_t    pm_int8_t;
+typedef uint8_t   pm_uint8_t;
+typedef int16_t   pm_int16_t;
+typedef uint16_t  pm_uint16_t;
+typedef int32_t   pm_int32_t;
+typedef uint32_t  pm_uint32_t;
+
+#endif /* __PGMTYPES__ */
+
